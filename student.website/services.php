374fa5f3feec9ac45026dfc46d4e5b2cf3772717
Shidan Wang <shidan.wang@utsouthwestern.edu>
2016-08-24 00:25:39 -0500

    Add file to student.website/
    index.php is the homepage

---
+++ student.website/services.php
@@ -0,0 +1,36 @@
+<section id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Services</h2>
+                <h3 class="section-subheading text-muted">Southlake Tutoring Academy provides you multiple choices of classes!</h3>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="service-heading">1-to-1 or small group class</h4>
+                <p class="text-muted">this is your introduction</p>
+            </div>
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="service-heading">Afterschool</h4>
+                <p class="text-muted">This is another introduction</p>
+            </div>
+            <div class="col-md-4">
+                    <span class="fa-stack fa-4x">
+                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                    </span>
+                <h4 class="service-heading">Camping</h4>
+                <p class="text-muted">More introduction</p>
+            </div>
+        </div>
+    </div>
+</section>
