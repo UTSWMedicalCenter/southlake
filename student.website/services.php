cf982dfd4b01b5e91422105b3e2b68d0c0ac491f
Shidan Wang <shidan.wang@utsouthwestern.edu>
2016-08-29 00:25:15 -0500

    update services.php and classes.php
    update icons in services.php; replace images and add tables to register page in classes.php

--- student.website/services.php
+++ student.website/services.php
@@ -10,26 +10,28 @@
             <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
                     </span>
                 <h4 class="service-heading">1-to-1 or small group class</h4>
-                <p class="text-muted">this is your introduction</p>
+                <p class="text-muted">We provide multiple types of classes for you! Language, subject, art/music,
+                    chess/pingpong, and kinds of exam preps... You can find every class you like here!</p>
             </div>
             <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-laptop fa-stack-1x fa-inverse"></i>
+                        <i class="fa fa-home fa-stack-1x fa-inverse"></i>
                     </span>
                 <h4 class="service-heading">Afterschool</h4>
-                <p class="text-muted">This is another introduction</p>
+                <p class="text-muted">Too busy to play with your child? Want to cultivate your child's communication
+                skills and so on? We are waiting for you!</p>
             </div>
             <div class="col-md-4">
                     <span class="fa-stack fa-4x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-lock fa-stack-1x fa-inverse"></i>
+                        <i class="fa fa-smile-o fa-stack-1x fa-inverse"></i>
                     </span>
                 <h4 class="service-heading">Camping</h4>
-                <p class="text-muted">More introduction</p>
+                <p class="text-muted">A colorful and fulfilling summer has been prepared for you and your children!</p>
             </div>
         </div>
     </div>
